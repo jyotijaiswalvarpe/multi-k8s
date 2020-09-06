b9632fc4b732da6b37dba8c83c45168a627f0eef
jj-code285 <jyotijaiswalvarpe@gmail.com>
2020-09-06 13:27:19 -0400

    deplyment script fixes

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
-docker build -t jyotijaiswal88/multi-client -f ./client/Dockerfile ./client
-docker build -t jyotijaiswal88/multi-server -f ./client/Dockerfile ./server
-docker build -t jyotijaiswal88/multi-worker -f ./client/Dockerfile ./worker
-docker push jyotijaiswal88/multi-client
-docker push jyotijaiswal88/multi-server
-docker push jyotijaiswal88/multi-worker
+docker build -t jyotijaiswal88/multi-client:latest -t jyotijaiswal88/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jyotijaiswal88/multi-server:latest -t jyotijaiswal88/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t jyotijaiswal88/multi-worker:latest -t jyotijaiswal88/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push jyotijaiswal88/multi-client:latest
+docker push jyotijaiswal88/multi-server:latest
+docker push jyotijaiswal88/multi-worker:latest
+
+docker push jyotijaiswal88/multi-client:$SHA
+docker push jyotijaiswal88/multi-server:$SHA
+docker push jyotijaiswal88/multi-worker:$SHA
+
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=jyotijaiswal88/multi-server:$SHA
 kubectl set image deployments/client-deployment client=jyotijaiswal88/multi-client:$SHA
